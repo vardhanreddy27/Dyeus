6a43a592972ecee0002a5809dbfb0887039f36c3
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-07-03 09:40:31 +0530

    banner updated

--- src/components/Bannner.js
+++ src/components/Bannner.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Bannner() {
   return (
-    <div className='text-center'><h1 className='about'>About Diana</h1>
+    <div className='text-center p-4 gradient'><h1 className='about'>About Diana</h1>
+    <h3 className='sub-text m-2 p-3'>A platform that looks out for you</h3>
+    <p className='para p-4 m-2'>Diana empowers you to understand how your body works so you can look and feel your best </p>
+    <hr></hr>
     </div>
   )
 }
