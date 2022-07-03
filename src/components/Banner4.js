97f49611cf0948899f1472b648bded914b3821cc
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2022-07-03 15:58:25 +0530

    banner2 updated

---
+++ src/components/Banner4.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Banner4() {
+  return (
+    <div className='text-center'><div className='container'><div className='row'><p className='para4 pt-5 ps-2 pe-2'>Track your mood
+    </p><p className='para6'>All days and all moods are not made equal. But we can only improve what we measure. Track your mood and vitals (including skin and hair health) so you can take better care of yourself every day. 
+</p>
+<div className='container'><div className='col ps-5 pe-5'><div className='row'><button type="button" className="btnblue btn btn-lg btn-block">Track you mood on the app</button></div>
+</div></div></div></div></div>
+  )
+}
+
+export default Banner4
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Banner2 from '../components/Banner2'
+import Banner3 from '../components/Banner3'
+import Banner4 from '../components/Banner4'
 import Bannner from '../components/Bannner'
 import Navbar from '../components/Navbar'
 function Home() {
@@ -6,6 +9,9 @@ function Home() {
 <>
  <Navbar />
  <Bannner />
+ <Banner2 />
+ <Banner3 />
+ <Banner4 />
 </>  )
 }
 
